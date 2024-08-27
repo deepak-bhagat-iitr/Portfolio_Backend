5a55e884bb2310346341eec24ea1f4653949257a
Deepak <deepak988088@gmail.com>
2024-08-27 20:27:00 +0530

    backend done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://portfolio-deepak-bhagat-git-main-deepak-bhagats-projects.vercel.app'
 }));
 
 app.use(express.json()); // Parse JSON bodies
